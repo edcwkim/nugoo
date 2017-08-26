5d20f8f477c11d279dba3db0abbcba5075b4f9f5
Ed Chanwoo Kim <edcwkim@gmail.com>
2017-08-26 13:34:42 +0900

    debug_toolbar middleware

--- nugoo/settings/__init__.py
+++ nugoo/settings/__init__.py
@@ -37,6 +37,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'nugoo.urls'
