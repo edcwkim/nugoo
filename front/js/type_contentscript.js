92f7e9d74b843b8b96241f3ba8c71d86a1ff52f6
JaesungHuh <gasop99@gmail.com>
2017-08-27 07:15:15 +0900

    Put ajax to script file

--- front/js/type_contentscript.js
+++ front/js/type_contentscript.js
@@ -1,128 +1,65 @@
-/*
 $(function() {
   $.ajax({
-    url: "https://nugoo.me/people/",
-    success: function(data) {
-      var names = data.data;
-      var flag = 0;
-      console.log(names);
-      // highlight all the names in names
-      for (var i = 0; i < names.length; ++i) {
-        var selector = "body :contains('" + names[i] + "')";
+      url="https://nugoo.me/people/names";
+      success: function(data) {
+          var names = data.data,
+              selector = "";
+          var flag = 0;
+          console.log(names);
+          
+          // highlight all the names in names
+          for (var i = 0; i < names.length; ++i) {
+            var selector = "body :contains('" + names[i] + "')";
 
-        $(selector).each(function(_, element) {
-          $(element).html(function(_, oldHtml) {
-            var regexp = new RegExp("(>[^<]*)" + names[i], "gmu"),
-                span = '<span class="nugoo nugoo-' + i + '">' + names[i] + '</span>';
-            return oldHtml.replace(regexp, "$1" + span);
-          });
-        });
-      }
-      
-      for (var i=0; i< names.length; ++i) {
-        $(".nugoo-" + i).tooltip({
-          items: ".nugoo-" + i,
-          content: '<iframe src="https://nugoo.me/p/' + names[i] + '/" width="400" height="224" style="border: none;"></iframe>',
-          classes: {"ui-tooltip": "nugoo-tooltip"}
-        });
-      }
-
-      var headers = $(".nugoo");
-      
-      $(headers).each(function(index) {
-        $(this).on("mouseenter", function (e) {
-            console.log("Before : " + flag);
-            if (flag == 0) {
-                e.stopImmediatePropagation();
-                $(this).tooltip("enable");
-                $(this).tooltip("open");
-                flag = 1
-                console.log("After : " + flag);
-            } else {
-                $(this).tooltip("disable");
-            }
-        });
-
-        $(this).on("mouseleave", function (e) {
-            e.stopImmediatePropagation();
-        });
-
-      });
-
-      $(document).mouseup(function (e) {
-          var container = $(".ui-tooltip");
-          if (! container.is(e.target) && 
-              container.has(e.target).length === 0)
-          {
-              $(".nugoo").tooltip("disable");
-              flag = 0;
-              console.log("Now : " + flag);
+            $(selector).each(function(_, element) {
+              $(element).html(function(_, oldHtml) {
+                var regexp = new RegExp("(>[^<]*)" + names[i], "gmu"),
+                    span = '<span class="nugoo nugoo-' + i + '">' + names[i] + '</span>';
+                return oldHtml.replace(regexp, "$1" + span);
+              });
+            });
           }
-      });
-        
-    }
+          
+          for (var i=0; i< names.length; ++i) {
+            $(".nugoo-" + i).tooltip({
+              items: ".nugoo-" + i,
+              content: '<iframe src="https://nugoo.me/p/' + names[i] + '/" width="400" height="224" style="border: none;"></iframe>',
+              classes: {"ui-tooltip": "nugoo-tooltip"}
+            });
+          }
+
+          var headers = $(".nugoo");
+          
+          $(headers).each(function(index) {
+            $(this).on("mouseenter", function (e) {
+                console.log("Before : " + flag);
+                if (flag == 0) {
+                    e.stopImmediatePropagation();
+                    $(this).tooltip("enable");
+                    $(this).tooltip("open");
+                    flag = 1
+                    console.log("After : " + flag);
+                } else {
+                    $(this).tooltip("disable");
+                }
+            });
+
+            $(this).on("mouseleave", function (e) {
+                e.stopImmediatePropagation();
+            });
+
+          });
+
+          $(document).mouseup(function (e) {
+              var container = $(".ui-tooltip");
+              if (! container.is(e.target) && 
+                  container.has(e.target).length === 0)
+              {
+                  $(".nugoo").tooltip("disable");
+                  flag = 0;
+                  console.log("Now : " + flag);
+              }
+          });
+      }
   });
-});
-*/
-
-$(function() {
- 
-      var names = ["박근혜", "조윤선", "김진태", "우병우"];
-      var flag = 0;
-      console.log(names.length);
-      // highlight all the names in names
-      for (var i = 0; i < names.length; ++i) {
-        var selector = "body :contains('" + names[i] + "')";
-
-        $(selector).each(function(_, element) {
-          $(element).html(function(_, oldHtml) {
-            var regexp = new RegExp("(>[^<]*)" + names[i], "gmu"),
-                span = '<span class="nugoo nugoo-' + i + '">' + names[i] + '</span>';
-            return oldHtml.replace(regexp, "$1" + span);
-          });
-        });
-      }
-      
-      for (var i=0; i< names.length; ++i) {
-        $(".nugoo-" + i).tooltip({
-          items: ".nugoo-" + i,
-          content: '<iframe src="https://nugoo.me/p/' + names[i] + '/" width="400" height="224" style="border: none;"></iframe>',
-          classes: {"ui-tooltip": "nugoo-tooltip"}
-        });
-      }
-
-      var headers = $(".nugoo");
-      
-      $(headers).each(function(index) {
-        $(this).on("mouseenter", function (e) {
-            console.log("Before : " + flag);
-            if (flag == 0) {
-                e.stopImmediatePropagation();
-                $(this).tooltip("enable");
-                $(this).tooltip("open");
-                flag = 1
-                console.log("After : " + flag);
-            } else {
-                $(this).tooltip("disable");
-            }
-        });
-
-        $(this).on("mouseleave", function (e) {
-            e.stopImmediatePropagation();
-        });
-
-      });
-
-      $(document).mouseup(function (e) {
-          var container = $(".ui-tooltip");
-          if (! container.is(e.target) && 
-              container.has(e.target).length === 0)
-          {
-              $(".nugoo").tooltip("disable");
-              flag = 0;
-              console.log("Now : " + flag);
-          }
-      });
-        
-
 });
