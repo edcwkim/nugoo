59b03e696960ded9639a912623139ccee6cbed77
Ed Chanwoo Kim <edcwkim@gmail.com>
2017-08-26 14:44:09 +0900

    apps.people models setup

--- nugoo/apps/people/models.py
+++ nugoo/apps/people/models.py
@@ -1,3 +1,102 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Hashtag(models.Model):
+    name = models.CharField(
+        '이름',
+        max_length=10,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Person(models.Model):
+    name = models.CharField(
+        '이름',
+        max_length=60,
+    )
+    events = models.ManyToManyField(
+        'Event',
+        through='PersonToEvent',
+        related_name='person_set',
+        verbose_name='사건들',
+        blank=True,
+    )
+
+    def get_photo_upload_to(self, filename):
+        return "{}/{}/{}.{}".format(
+            self.__class__.__name__.lower(),
+            'photo',
+            uuid.uuid4(),
+            filename.split(".")[-1],
+        )
+    photo = models.ImageField(
+        '사진',
+        upload_to=get_photo_upload_to,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class PersonToEvent(models.Model):
+    person = models.ForeignKey(
+        Person,
+        models.SET_NULL,
+        related_name='event_throughs',
+        verbose_name='인물',
+        blank=True,
+        null=True,
+    )
+    event = models.ForeignKey(
+        'Event',
+        models.SET_NULL,
+        related_name='person_throughs',
+        verbose_name='사건',
+        blank=True,
+        null=True,
+    )
+    hashtags = models.ManyToManyField(
+        Hashtag,
+        related_name='person_to_event_set',
+        verbose_name='해시태그',
+        blank=True,
+    )
+
+    def __str__(self):
+        return "{} : {}".format(self.person, self.event)
+
+
+class Event(models.Model):
+    name = models.CharField(
+        '이름',
+        max_length=255,
+    )
+    reference = models.URLField(
+        '참조 자료',
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Stat(models.Model):
+    person = models.ForeignKey(
+        Person,
+        models.SET_NULL,
+        related_name='stat_set',
+        verbose_name='인물',
+        blank=True,
+        null=True,
+    )
+    name = models.CharField(
+        '이름',
+        max_length=10,
+    )
+    value = models.SmallIntegerField(
+        '능력치 값',
+    )
