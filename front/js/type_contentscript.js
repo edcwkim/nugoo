184e1e56d5c3bc79d1d564f3a80ad955146d5b7d
JaesungHuh <gasop99@gmail.com>
2017-08-26 22:43:48 +0900

    Get the data from server and highlight the name

--- front/js/type_contentscript.js
+++ front/js/type_contentscript.js
@@ -1,31 +1,102 @@
 /*
 $(function() {
-  $.ajax({
-    url: "https://b31a79b7.ngrok.io/people/names/",
-    success: function(data) {
-      var names = data.data,
-          selector = "";
-	  
-	  console.log("names "+names);
-      for (var i = 0; i < names.length; ++i)
-        selector += ":contains('"+ names[i] + "'), ";
-	
-      if (selector.length)
-        selector = selector.slice(0, -2);
-      console.log(selector);
 
-      $("body").contents()
-               .filter(selector)
-               .wrap("<b></b>");
+	var names = ["이재용", "이건희", "이혁"],
+		selector = "";
+  
+	console.log("names "+names);
+	
+	// highlight all the names in names
+	for (var i = 0; i < names.length; ++i) {
+		selector = ":contains('"+ names[i] + "')";
+		console.log(selector);  
+	
+		$("body" + selector).html(function(_, html) {
+			return html.split(names[i]).join("<span class='nugoo' id='nugoo" + names[i] + "' >" + names[i] + "</span>");
+		});
+	}
+	
+	// tooltip function
+	for (var i = 0; i < names.length; ++i) {
+		selector = ":contains('"+ names[i] + "')";
+		console.log(selector);  
+
+		$("#nugoo"+names[i]).tooltip({
+			items : "#nugoo" + names[i],
+			//content : "<iframe src='https://www.w3schools.com'></iframe>"
+			content : "<b>" + names[i] + "내용</b>"
+		});
+	}
+
+});
+*/
+/*
+$(function() {
+
+	var names = ["이재용", "이건희", "이혁"],
+		selector = "";
+  
+	console.log("names "+names);
+	
+	// highlight all the names in names
+	for (var i = 0; i < names.length; ++i) {
+		selector = ":contains('"+ names[i] + "')";
+		console.log(selector);  
+	
+		$("body" + selector).html(function(_, html) {
+			return html.split(names[i]).join("<span class='nugoo"+ names[i] + "' >" + names[i] + "</span>");
+		});
+	}
+	
+	// tooltip function
+	for (var i = 0; i < names.length; ++i) {
+		selector = ":contains('"+ names[i] + "')";
+		console.log(selector);  
+
+		$(".nugoo"+names[i]).tooltip({
+			items : ".nugoo" + names[i],
+			//content : "<iframe src='https://www.w3schools.com'></iframe>"
+			content : "<b>" + names[i] + "내용</b>"
+		});
+	}
+
+});
+*/
+
+
+
+$(function() {
+  $.ajax({
+    url: "https://nugoo.me/people/names/",
+    success: function(data) {
+ 	var names = data.data,
+		selector = "";
+  
+	console.log("names "+names);
+	
+	// highlight all the names in names
+	for (var i = 0; i < names.length; ++i) {
+		selector = ":contains('"+ names[i] + "')";
+		console.log(selector);  
+	
+		$("body" + selector).html(function(_, html) {
+			return html.split(names[i]).join("<span class='nugoo' id='nugoo" + names[i] + "' >" + names[i] + "</span>");
+		});
+	}
+	
+	// tooltip function
+	for (var i = 0; i < names.length; ++i) {
+		selector = ":contains('"+ names[i] + "')";
+		console.log(selector);  
+
+		$("#nugoo"+names[i]).tooltip({
+			items : "#nugoo" + names[i],
+			//content : "<iframe src='https://www.w3schools.com'></iframe>"
+			content : "<b>" + names[i] + "내용</b>"
+		});
+	}
+
     }
   });
 });
-*/
-$(function() {
 
-	$("body:contains('이재용')").html(function(_, html) {
-		return html.split('이재용').join("<span class='nugu'>이재용</span>");
-	});
-	//var replace_str = $('body').html().replace(/contracting/g, '<strong>contracting</strong>'); 
-	//$('body').html(replace_str);
-});
