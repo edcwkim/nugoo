443da8807c4cf6e21572c795dea6a15902dcf8ff
Ed Chanwoo Kim <edcwkim@gmail.com>
2017-08-26 18:16:46 +0900

    Sqlite to Postgres

--- nugoo/settings/__init__.py
+++ nugoo/settings/__init__.py
@@ -74,8 +74,12 @@ INTERNAL_IPS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nugoo',
+        'USER': 'ubuntu',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
