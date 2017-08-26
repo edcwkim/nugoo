69ff31e64eaf27ca126f3ea4c1420f8f6c46fcc8
JaesungHuh <gasop99@gmail.com>
2017-08-26 15:58:52 +0900

    Sample tooltip

--- front/js/type_contentscript.js
+++ front/js/type_contentscript.js
@@ -1,18 +1,4 @@
-var arrayOfNumbers = [];
-alert("hi");
-var regex =  /\d*[/-]*[0-9][0-9][0-9][/ -]*[0-9][0-9][0-9][/ -]*[0-9][0-9][0-9][0-9][ ]*/g;
-newBody = document.body.innerHTML;
-var i = 0;
-do
-{
-    temp = regex.exec(newBody);
-    if (temp != null)
-        arrayOfNumbers[i] = temp;
-    i++
-}
-while (temp)
-for (var i = 0; i < arrayOfNumbers.length; i++)
-{
-    newBody = newBody.replace(arrayOfNumbers[i], "<a href='http://www.google.com'>" + arrayOfNumbers[i] + "</a>");
-}
-document.body.innerHTML = newBody;
+alert('hi');
+$(function() {
+    $(document).tooltip();
+});
