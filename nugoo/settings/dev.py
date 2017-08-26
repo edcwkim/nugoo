0832392bed6ae72f563e5cb81def95ec70c8fb12
Ed Chanwoo Kim <edcwkim@gmail.com>
2017-08-26 13:29:55 +0900

    Project initialization

---
+++ nugoo/settings/dev.py
@@ -0,0 +1,13 @@
+from . import *  # noqa
+
+# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'i6^5svhg#&&5ha2sj9hv18n3*2*bw1g$mc+#+m$h1rvczg3lz&'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+WSGI_APPLICATION = None
