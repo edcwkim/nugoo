fe9d649045c658e097e7c9480e9883f1da9379a6
JaesungHuh <gasop99@gmail.com>
2017-08-26 20:14:35 +0900

    add style.css, detect 이재용

--- front/js/type_contentscript.js
+++ front/js/type_contentscript.js
@@ -1,12 +1,15 @@
+/*
 $(function() {
   $.ajax({
     url: "https://b31a79b7.ngrok.io/people/names/",
     success: function(data) {
       var names = data.data,
           selector = "";
-
+	  
+	  console.log("names "+names);
       for (var i = 0; i < names.length; ++i)
         selector += ":contains('"+ names[i] + "'), ";
+	
       if (selector.length)
         selector = selector.slice(0, -2);
       console.log(selector);
@@ -16,4 +19,13 @@ $(function() {
                .wrap("<b></b>");
     }
   });
+});
+*/
+$(function() {
+
+	$("body:contains('이재용')").html(function(_, html) {
+		return html.split('이재용').join("<span class='nugu'>이재용</span>");
+	});
+	//var replace_str = $('body').html().replace(/contracting/g, '<strong>contracting</strong>'); 
+	//$('body').html(replace_str);
 });
