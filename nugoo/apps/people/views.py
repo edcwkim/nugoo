ee5fabd65e1c5e4ee1309846444e2afb937afbd7
Ed Chanwoo Kim <edcwkim@gmail.com>
2017-08-26 18:01:47 +0900

    Person list views

--- nugoo/apps/people/views.py
+++ nugoo/apps/people/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.views import generic
 from .models import Person
 
@@ -6,3 +7,21 @@ class PersonDetail(generic.DetailView):
     model = Person
     context_object_name = 'person'
     template_name = 'people/person-detail.html.dj'
+
+
+class PersonListByName(generic.ListView):
+    context_object_name = 'people'
+    template_name = 'people/person-list-by-name.html.dj'
+
+    def get_queryset(self):
+        return Person.objects.filter(name=self.kwargs['name'])
+
+
+class PersonListName(generic.ListView):
+    model = Person
+
+    def get(self, request, *args, **kwargs):
+        names = self.get_queryset().values_list('name', flat=True)
+        return JsonResponse({
+            'data': list(names),
+        })
